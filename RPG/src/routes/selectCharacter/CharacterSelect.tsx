d04baad33e7f414bdf57ed52f179f03ea69ff2c9
RudyMathis <rudyjmathis@gmail.com>
2025-03-31 16:03:20 -0400

    connected with java and level up mechanic

--- RPG/src/routes/selectCharacter/CharacterSelect.tsx
+++ RPG/src/routes/selectCharacter/CharacterSelect.tsx
@@ -3,15 +3,25 @@ import NavigateBtn from "../../components/NavigateBtn";
 import "./CharacterSelect.css";
 import { GameLevelAtom } from "../../atom/GameLevelAtom";
 import { useAtom } from "jotai";
+// import { useEffect, useState } from "react";
 
 const CharacterSelect = () => {
 
     const [currentGameLevel] = useAtom(GameLevelAtom);
+    // const [message, setMessage] = useState<string>('');
+
+    // useEffect(() => {
+    //     fetch('http://localhost:8080/api/greeting')
+    //         .then(response => response.text())
+    //         .then(data => setMessage(data))
+    //         .catch(error => console.error('Error fetching data:', error));
+    // }, []);
 
     return (
         <>
             <div className="main-container">           
                 <h1>Select Your Character</h1>
+                {/* <h2>{message}</h2> */}
                 <NavigateBtn locationValue="/" location="Main Menu" />
             </div>
             <CharacterSheets />
